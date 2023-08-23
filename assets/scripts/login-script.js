ab4d03629eb84d55bed98ba64d608f7b6a58ec80
Rina057 <142887326+Rina057@users.noreply.github.com>
2023-08-23 08:49:22 +0700

    Update login-script.js

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -39,21 +39,21 @@ loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
 
 /** 
-   * Membuat variabel email untuk menyimpan nilai email yang didapatkan saat button ditekan.
-   * @constant {string}
-   */
-  const email = inputEmailElement.value;
-  /** 
-   * Membuat variabel password untuk menyimpan niali password yang didapatkan saat button ditekan.
-   * @constant {string}
-   */
-  const password = inputPasswordElement.value;
+* Membuat variabel email untuk menyimpan nilai email yang didapatkan saat button ditekan.
+* @constant {string}
+*/
+const email = inputEmailElement.value;
+/** 
+* Membuat variabel password untuk menyimpan niali password yang didapatkan saat button ditekan.
+* @constant {string}
+*/
+const password = inputPasswordElement.value;
 /* Comment : Memastikan bahwa nilai email dan password sesuai dengan nilai yang tersimpan. */
-  if (email == expectedEmail && password == expectedPassword) 
+if (email == expectedEmail && password == expectedPassword) 
 /* Comment : Jika sesuai maka program akan berpindah ke halaman home. */
-  {
-    goToHome();
-  } 
+{
+  goToHome();
+} 
 /* Comment : Namun jika tidak sesuai maka akan menampilkan informasi bahwa input salah. */
   else {
     showPopUp();
